76f23fa39ca9846c4897fb14dfd6c54d28886954
Sasha <alexanderyovbak@gmail.com>
2021-08-13 11:20:54 +0300

    new project

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-let number = '5';
-let realNumber = +number;
-console.log(realNumber);
+let numberOfFlims = prompt('Сколько фильмов вы посмотрели?');
+
+let personalMovieDB = {
+    count: numberOfFlims,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let movieName = prompt('Один из последних просмотренных фильмов?');
+let movieRating = prompt('На сколько оцените его?');
+personalMovieDB.movies.movieName = movieRating;
+
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movies.movieName);
+console.log(personalMovieDB.movies)
